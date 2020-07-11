4a204a1b12a7f09c486fd477420ce2b4a8722d8f
Patrick <pwcarney@bu.edu>
2020-07-10 21:24:54 -0400

    Added two test switches

---
+++ Assets/Scripts/Interior/TwoModeSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoModeSwitch : MonoBehaviour
+{
+    bool flipped = false;
+    public List<Sprite> sprites;
+
+    private void OnMouseDown()
+    {
+        flipped = !flipped;
+        GetComponent<AudioSource>().Play();
+
+        if (flipped)
+        {
+            GetComponent<SpriteRenderer>().sprite = sprites[0];
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = sprites[1];
+        }
+    }
+}
