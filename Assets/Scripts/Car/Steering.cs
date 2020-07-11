bd100f8c5b15d14fb782605743b5cff3c421150c
Patrick <pwcarney@bu.edu>
2020-07-11 09:45:10 -0400

    Basic steering is in

---
+++ Assets/Scripts/Car/Steering.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steering : MonoBehaviour
+{
+    public GameObject wheel;
+    private Quaternion orig_rot;
+
+    public GameObject car;
+    private float lateral_velocity = 10;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        orig_rot = wheel.transform.rotation;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Input.GetAxis("Mouse X") < 0)
+            {
+                wheel.transform.Rotate(new Vector3(0, -80 * Time.deltaTime, 0));
+                car.transform.Translate(new Vector3(-lateral_velocity * Time.deltaTime, 0, 0));
+            }
+            if (Input.GetAxis("Mouse X") > 0)
+            {
+                wheel.transform.Rotate(new Vector3(0, 80 * Time.deltaTime, 0));
+                car.transform.Translate(new Vector3(lateral_velocity * Time.deltaTime, 0, 0));
+            }
+        }
+        else
+        {
+            wheel.transform.rotation = orig_rot;
+        }
+    }
+}
