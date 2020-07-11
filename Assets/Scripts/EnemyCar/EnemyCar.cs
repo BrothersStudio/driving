8d99af4a5f07e161e55719504ebe1491dab139fe
jkcarney <joshuakcarney@gmail.com>
2020-07-11 15:45:22 -0400

    Added box collider to cars. Enemy cars will no longer collide with different speeds

--- Assets/Scripts/EnemyCar/EnemyCar.cs
+++ Assets/Scripts/EnemyCar/EnemyCar.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class EnemyCar : MonoBehaviour
 {
-    private float speed = 20;
+    private float speed;
 
     public List<Material> paint_colors;
 
     private void Start()
     {
+        speed = Random.Range(15.0f, 30.0f);
         Material paint_chosen = paint_colors[Random.Range(0, paint_colors.Count)];
 
         List<Material> materials = transform.Find("carrosserie").GetComponent<MeshRenderer>().materials.ToList();
@@ -40,10 +41,35 @@ public class EnemyCar : MonoBehaviour
 
     private void Update()
     {
+        
         transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime), Space.World);
         if (transform.position.z < -100)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate() 
+    {
+        PreventCollision();    
+    }
+
+    public float getSpeed()
+    {
+        return speed;
+    }
+
+    private void PreventCollision()
+    {
+        Debug.DrawLine(new Vector3(transform.position.x, transform.position.y + 0.7f, transform.position.z), new Vector3(transform.position.x, transform.position.y + 0.7f, transform.position.z + 10f));
+        RaycastHit hit;
+        if(Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 0.7f, transform.position.z), transform.TransformDirection(Vector3.forward), out hit, 10f))
+        {
+            float colliderSpeed = hit.collider.GetComponent<EnemyCar>().getSpeed();
+            if(colliderSpeed != this.speed)
+            {
+                this.speed = colliderSpeed;
+            }
         }
     }
 }
