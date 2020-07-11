8c68c917083d35fe8565806a3a1a33da5a7a4d9b
Patrick <pwcarney@bu.edu>
2020-07-11 13:59:14 -0400

    Enemy cars added

---
+++ Assets/Scripts/EnemyCar/EnemyCar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemyCar : MonoBehaviour
+{
+    private float speed = 20;
+
+    public List<Material> paint_colors;
+
+    private void Start()
+    {
+        Material paint_chosen = paint_colors[Random.Range(0, paint_colors.Count)];
+
+        List<Material> materials = transform.Find("carrosserie").GetComponent<MeshRenderer>().materials.ToList();
+        materials[0] = paint_chosen;
+        transform.Find("carrosserie").GetComponent<MeshRenderer>().materials = materials.ToArray();
+
+        materials = transform.Find("Animatable/door_front_L").GetComponent<MeshRenderer>().materials.ToList();
+        materials[0] = paint_chosen;
+        transform.Find("Animatable/door_front_L").GetComponent<MeshRenderer>().materials = materials.ToArray();
+
+        materials = transform.Find("Animatable/door_front_R").GetComponent<MeshRenderer>().materials.ToList();
+        materials[0] = paint_chosen;
+        transform.Find("Animatable/door_front_R").GetComponent<MeshRenderer>().materials = materials.ToArray();
+
+        materials = transform.Find("Animatable/hood").GetComponent<MeshRenderer>().materials.ToList();
+        materials[0] = paint_chosen;
+        transform.Find("Animatable/hood").GetComponent<MeshRenderer>().materials = materials.ToArray();
+
+        materials = transform.Find("Animatable/Trunk").GetComponent<MeshRenderer>().materials.ToList();
+        materials[0] = paint_chosen;
+        transform.Find("Animatable/Trunk").GetComponent<MeshRenderer>().materials = materials.ToArray();
+
+        materials = transform.Find("Animatable/Trunk/spoiler").GetComponent<MeshRenderer>().materials.ToList();
+        materials[0] = paint_chosen;
+        transform.Find("Animatable/Trunk/spoiler").GetComponent<MeshRenderer>().materials = materials.ToArray();
+    }
+
+    private void Update()
+    {
+        transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime), Space.World);
+        if (transform.position.z < -100)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyCar/EnemyCarSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCarSpawner : MonoBehaviour
+{
+    private float cooldown = 5;
+    private float last_spawn = -5;
+    public GameObject enemy_car;
+    private float[] spawn_x = { -5, 0, 5 };
+
+    private void Start()
+    {
+        SpawnCars(200f);
+        SpawnCars(300f);
+        SpawnCars(400f);
+        SpawnCars(500f);
+        SpawnCars(600f);
+        SpawnCars(700f);
+    }
+
+    private void Update()
+    {
+        if (Time.timeSinceLevelLoad > last_spawn + cooldown)
+        {
+            SpawnCars(800);
+        }
+    }
+
+    private void SpawnCars(float dist)
+    {
+        last_spawn = Time.timeSinceLevelLoad;
+        Instantiate(enemy_car, new Vector3(spawn_x[Random.Range(0, spawn_x.Length)], 0, dist), Quaternion.identity, transform);
+        if (Random.Range(0f, 1f) < 0.5f)
+        {
+            Instantiate(enemy_car, new Vector3(spawn_x[Random.Range(0, spawn_x.Length)], 0, dist), Quaternion.identity, transform);
+        }
+    }
+}
