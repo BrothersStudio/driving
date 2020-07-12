139e8b7665fdf90ea6ee181da5deac454c5f9a23
jkcarney <joshuakcarney@gmail.com>
2020-07-12 10:19:26 -0400

    Added combo loss sfx, combo bar sfx needs to be dropped in

--- Assets/Scripts/UI/ComboBar.cs
+++ Assets/Scripts/UI/ComboBar.cs
@@ -5,6 +5,8 @@ using TMPro;
 
 public class ComboBar : MonoBehaviour
 {
+    public AudioSource combo_loss_sfx;
+
     private GameObject bar;
     private float decrease_speed = 0.070f;
     private bool animating = false;
@@ -25,7 +27,7 @@ public class ComboBar : MonoBehaviour
     {
         animating = true;
         bar.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
-
+        combo_loss_sfx.Play();
         transform.parent.Find("Multiplier").GetComponent<TMP_Text>().text = combo.ToString() + "x";
         transform.parent.Find("Multiplier").GetComponent<TMP_Text>().enabled = true;
     }
@@ -42,6 +44,7 @@ public class ComboBar : MonoBehaviour
                 animating = false;
 
                 FindObjectOfType<Score>().ComboOver();
+
                 transform.parent.Find("Multiplier").GetComponent<TMP_Text>().text = "";
                 transform.parent.Find("Multiplier").GetComponent<TMP_Text>().enabled = false;
             }
