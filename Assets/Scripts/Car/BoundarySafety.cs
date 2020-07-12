f3e19303e886a2bf8af504823332b70094a74ee6
Patrick <pwcarney@bu.edu>
2020-07-11 21:05:04 -0400

    Added script to put car back at 0,0,0 if they are OOB

---
+++ Assets/Scripts/Car/BoundarySafety.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundarySafety : MonoBehaviour
+{
+    private void Update()
+    {
+        if (transform.position.x > 10.5f || transform.position.x < -10.5f)
+        {
+            transform.position = Vector3.zero;
+        }
+    }
+}
