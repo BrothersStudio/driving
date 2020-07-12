069a621b3053f21a8e847be77b897088c1afa7d1
jkcarney <joshuakcarney@gmail.com>
2020-07-12 09:54:22 -0400

    Combo multiplier goes away once done. Prelim scoreboard structure

---
+++ Assets/Scripts/UI/Scoreboard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour
+{
+    private float latest_player_score;
+    private float highest_player_score;
+
+    private string player_name = "You";
+
+    private List<(string,float)> saved_scores = new List<(string, float)>();
+
+    private void Start() 
+    {
+        InitializeFakeScores();    
+    }
+
+    void UpdateScores(float new_score)
+    {
+        latest_player_score = new_score;
+        if(new_score > highest_player_score)
+        {
+            highest_player_score = new_score;
+        }
+    }
+
+    void InitializeFakeScores()
+    {
+        saved_scores.Add(("Patrick", 235000000));
+        saved_scores.Add(("Joshua", 220500000));
+        saved_scores.Add(("Kevin", 195000000));
+        saved_scores.Add(("Christopher", 104000000));
+        
+    }
+}
